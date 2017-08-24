d65acd475e1b36873d582da3b785b457736f9fe9
hecter_wu <hecter_wu@transcend.com.tw>
2017-08-24 17:52:21 +0800

    testcase: testDot by using p[j] is "."

--- Regular Expression Matching.py
+++ Regular Expression Matching.py
@@ -17,13 +17,18 @@ class unitest(unittest.TestCase):
         p = "aa"
         Ans = False
         self.assertEqual(Solution().isMatch(s,p),Ans);
+    def testDot(self):
+        s = "ac"
+        p = "a."
+        Ans = True
+        self.assertEqual(Solution().isMatch(s,p),Ans);
 
 class Solution():
     def isMatch(self, s, p):
         s = s[::-1]
         p = p[::-1]
         for i,j in itertools.zip_longest(range(len(s)), range(len(p))):
-            if i is None or j is None or s[i] != p[j]:
+            if i is None or j is None or (s[i] != p[j] and p[j] != "."):
                 return False
         return True
 
